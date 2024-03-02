c79f07750f3440939b6de268f060efc236ad12cb
Kurt Harriger <kurtharriger@gmail.com>
2024-03-02 09:07:10 -0700

    new project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kurtharriger.clj-mergetool
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kurtharriger/clj_mergetool_test.clj
@@ -0,0 +1,7 @@
+(ns kurtharriger.clj-mergetool-test
+  (:require [clojure.test :refer :all]
+            [kurtharriger.clj-mergetool :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
